fbb894eeb83a1485e060e56bb000d4fe4d6a7fea
Shobhit Bhatnagar <shbhatna@iu.edu>
2019-09-18 11:16:53 -0400

    In Class work - form code in html and vue code in recordapp

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,13 +1,34 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    patients: []
+    patients: [],
+    formPatients:{
+      firstName:"",
+      lastName:"",
+      dob:"",
+      sexAtBirth:"",
+    }
   },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
       .then(json => { patientRecordApp.patients = json })
+    },
+    handleCreateRecord(event){
+      // problem - that the data is not stored
+      //form is using the same instance
+    //  fetch(url, {method:'post', data:thisFormPatient})
+      //.then()
+      this.patients.push(this.formPatients);
+      this.formPatients={
+        formPatients:{
+          firstName:"",
+          lastName:"",
+          dob:"",
+          sexAtBirth:"",
+        }
+      }
     }
   },
   created() {
