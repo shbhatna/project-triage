000b615722d410270c366aa8ae20df66a3ef8608
Shobhit Bhatnagar <shbhatna@iu.edu>
2019-10-14 09:13:43 -0400

    InClass 10-14 MomentJs Library

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -8,8 +8,16 @@ var waitingApp = new Vue({
       fetch('api/waiting/')
       .then(response => response.json())
       .then(json => { waitingApp.patients = json })
+    },
+    formatLocalVisitDate(d) {
+    return  moment.utc(d), local(), format("HH:mm MMM Do");//24hour Time + month & date
+    //need to have return to ensure that the method returns a VALUES
     }
   },
+  sinceLocalVisistDate(d){
+    return moment,utc(d).local().fromNow();
+  }
+},
   created() {
     this.fetchPatients();
   }
